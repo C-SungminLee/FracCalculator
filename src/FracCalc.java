fde83dca0aca64130dc0e4782b927bee54b71fdc
C-SungminLee <91634957+C-SungminLee@users.noreply.github.com>
2022-11-28 09:28:40 -0800

    Update FracCalc.java

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -15,60 +15,137 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) {
         // TODO: Implement this function to produce the solution to the input
         Scanner scan = new Scanner(input);
         scan.useDelimiter(" ");
-        scan.next();
-        scan.next();
+        String firstFraction = (scan.next());
+        String operator = (scan.next());
         String secondFraction = (scan.next());
 
         //return (secondFraction);
-        return("whole:"+findWhole(secondFraction)+" numerator:"+findNumerator(secondFraction)+" denominator:"+findDenomentaor(secondFraction));
+        //return("whole:"+findWhole(secondFraction)+" numerator:"+findNumerator(secondFraction)+" denominator:"+findDenomenator(secondFraction));
+
+
+
+        int firstFractionWhole = Integer.parseInt(findWhole(firstFraction));
+        int firstFractionNumerator = Integer.parseInt(findNumerator(firstFraction));
+        int firstFractionDenomenator = Integer.parseInt(findDenomenator(firstFraction));
+        int secondFractionWhole = Integer.parseInt(findWhole(secondFraction));
+        int secondFractionNumerator = Integer.parseInt(findNumerator(secondFraction));
+        int secondFractionDenomenator = Integer.parseInt(findDenomenator(secondFraction));
+        String finalnumber = "";
+
+        //checkpoint 3
+        if (operator.equals("+")) {
+            //addition
+            finalnumber = (((secondFractionDenomenator * firstFractionNumerator)+( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole+secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator))+"/"+(firstFractionDenomenator*secondFractionDenomenator));
+        } else if (operator.equals("-")) {
+            //subtraction
+            finalnumber =(((secondFractionDenomenator * firstFractionNumerator)-( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole-secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator))+"/"+(firstFractionDenomenator*secondFractionDenomenator));
+        } else if (operator.equals("*")) {
+            finalnumber = ((((firstFractionWhole * firstFractionDenomenator) + (firstFractionNumerator)) * ((secondFractionWhole * secondFractionDenomenator) + (secondFractionNumerator))) + "/" + (firstFractionDenomenator * secondFractionDenomenator));
+        } else if (operator.equals("/")) {
+            //division
+            finalnumber = (((firstFractionWhole * firstFractionDenomenator) + (firstFractionNumerator)) * (secondFractionDenomenator) + "/" + (firstFractionDenomenator) * ((secondFractionWhole * secondFractionDenomenator) + secondFractionNumerator));
         }
 
+        return finalnumber;
+    }
+
+
+        /*
+        // Final
+        if (operator.equals("+")) {
+            //addition
+            if (!(("" +(secondFractionDenomenator * firstFractionNumerator)+( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole+secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator))).equals("0")){
+                finalnumber = (((secondFractionDenomenator * firstFractionNumerator)+( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole+secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator))+"/"+(firstFractionDenomenator*secondFractionDenomenator));
+            }
+            if ((firstFractionDenomenator*secondFractionDenomenator) == 1){
+                finalnumber =(""+ ((secondFractionDenomenator * firstFractionNumerator)+( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole+secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator)));
+            }
+            else{
+                finalnumber = "0";
+            }
+            if (finalnumber.charAt(0) == 0 && finalnumber.charAt(1)==0){
+                finalnumber = finalnumber.substring(2,finalnumber.length());
+            }
+        } else if (operator.equals("-")) {
+            //subtraction
+            finalnumber =(((secondFractionDenomenator * firstFractionNumerator)-( firstFractionDenomenator * secondFractionNumerator)+(firstFractionWhole-secondFractionWhole)*(firstFractionDenomenator*secondFractionDenomenator))+"/"+(firstFractionDenomenator*secondFractionDenomenator));
+        } else if (operator.equals("*")) {
+            //multiplication
+            if ((((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*((secondFractionWhole*secondFractionDenomenator)+(secondFractionNumerator))) == 0){
+                finalnumber = "0";
+            }else if ((firstFractionDenomenator*secondFractionDenomenator) == 1){
+                finalnumber = (""+(((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*((secondFractionWhole*secondFractionDenomenator)+(secondFractionNumerator))));
+            }else{
+                if ((((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*((secondFractionWhole*secondFractionDenomenator)+(secondFractionNumerator))) % (firstFractionDenomenator*secondFractionDenomenator) == 0){
+                    finalnumber = ("" + (((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*((secondFractionWhole*secondFractionDenomenator)+(secondFractionNumerator)))/(firstFractionDenomenator*secondFractionDenomenator));
+                }else {
+                    finalnumber = ((((firstFractionWhole * firstFractionDenomenator) + (firstFractionNumerator)) * ((secondFractionWhole * secondFractionDenomenator) + (secondFractionNumerator))) + "/" + (firstFractionDenomenator * secondFractionDenomenator));
+                }
+            }
+        } else if (operator.equals("/")) {
+            //division
+            if (((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*(secondFractionDenomenator) == 0){
+                finalnumber = "0";
+            }else if(((firstFractionWhole*firstFractionDenomenator)+(firstFractionNumerator))*(secondFractionDenomenator) == ((firstFractionDenomenator) * ((secondFractionWhole * secondFractionDenomenator) + secondFractionNumerator))){
+                finalnumber = "1";
+            }else {
+                finalnumber = (((firstFractionWhole * firstFractionDenomenator) + (firstFractionNumerator)) * (secondFractionDenomenator) + "/" + (firstFractionDenomenator) * ((secondFractionWhole * secondFractionDenomenator) + secondFractionNumerator));
+            }
+            }
+        return finalnumber;
+    }*/
+
+
+
     // TODO: Fill in the space below with any helper methods that you think you will need
-    public static String findWhole(String secondFraction){
+    public static String findWhole(String fraction){
         String whole = "0";
-        if (secondFraction.contains("/")){
-            if (secondFraction.contains("_")){
-                Scanner scan = new Scanner(secondFraction);
+        if (fraction.contains("/")){
+            if (fraction.contains("_")){
+                Scanner scan = new Scanner(fraction);
                 scan.useDelimiter("_");
                 whole = scan.next();
             }
         }else{
-            whole = secondFraction;
+            whole = fraction;
         }
         return(whole);
     }
-    public static String findNumerator(String secondFraction){
+    public static String findNumerator(String fraction){
         String numerator = "0";
-        if (secondFraction.contains("/")){
-            if (secondFraction.contains("_")){
-                Scanner scan = new Scanner(secondFraction);
+        if (fraction.contains("/")){
+            if (fraction.contains("_")){
+                Scanner scan = new Scanner(fraction);
                 scan.useDelimiter("/");
                 Scanner secondScan = new Scanner(scan.next());
                 secondScan.useDelimiter("_");
-                secondScan.next();
-                numerator = secondScan.next();
+                if ((secondScan.next()).charAt(0) == '-'){
+                    numerator="-";
+                }
+                numerator += secondScan.next();
+
             }else{
-                Scanner scan = new Scanner(secondFraction);
+                Scanner scan = new Scanner(fraction);
                 scan.useDelimiter("/");
                 numerator = scan.next();
             }
         }
         return(numerator);
     }
-    public static String findDenomentaor(String secondFraction){
-        String Denomenator = "1";
-        if (secondFraction.contains("/")){
-            Scanner scan = new Scanner(secondFraction);
+    public static String findDenomenator(String fraction){
+        String denomenator = "1";
+        if (fraction.contains("/")){
+            Scanner scan = new Scanner(fraction);
             scan.useDelimiter("/");
             scan.next();
-            Denomenator = scan.next();
+            denomenator = scan.next();
+
         }
-        return(Denomenator);
+        return(denomenator);
     }
 
 }
